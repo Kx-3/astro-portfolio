fb58b4e628cbe154bac8f1cc77414de8e5a95138
Kipkirui Keter <kipkiruiketer755@gmail.com>
2026-07-23 16:53:05 +0800

    feat: added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,14 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://kipkirui.dev",
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 });
